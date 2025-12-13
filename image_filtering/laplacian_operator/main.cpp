2afcc83463902478dfc4c927d9fe529575af20a3
Muhammad Tarek <medojudoman@gmail.com>
2025-12-13 13:40:15 +0200

    add laplacian operator

--- image_filtering/laplacian_operator/main.cpp
+++ image_filtering/laplacian_operator/main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cv::Mat img{cv::imread("/home/muhammad/Downloads/32721009.jpeg")};
+    cv::Mat out;
+    cv::cvtColor(img,img,cv::COLOR_BGR2RGB);
+    int k_size=3,factor=1,delta=1;
+    cv::Laplacian(img,out,-1,k_size,factor,delta);
+    cv::imshow("test",out);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
     return 0;
 }
